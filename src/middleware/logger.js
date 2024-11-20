3a39bd308449688cbad1057a9ddd041cef6358f7
Mattazz <matthewazada101@gmail.com>
2024-11-20 16:17:05 -0500

    add logging functionality to track user interactions and save logs to file

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
 function logUserInteraction(msg){
+    const dateTime = new Date();
     const chatId = msg.chat.id;
     const userId = msg.from.id;
     const userName = msg.from.username || `${msg.from.first_name} ${msg.from.last_name}`;
@@ -6,6 +10,51 @@ function logUserInteraction(msg){
 
     console.log(`User Interaction - Chat ID: ${chatId}, User ID: ${userId}, Username: ${userName}, Message: ${messageText}`);
 
+    // save log as json
+    const log = {
+        dateTime,
+        chatId,
+        userId,
+        userName,
+        messageText
+    }
+    
+    // Save logs to a file
+
+}
+
+function saveLogToFile(log){
+    // Save logs to a file
+    const dateToday = new Date().toISOString().split('T')[0];
+
+    const logFilePath = path.join(__dirname,`user_interaction${dateToday}.log`)
+    const logString = JSON.stringify(log) + '\n'; // NDJSON format
+
+
+    if (dateToday !== log.dateTime.toISOString().split('T')[0]) {
+        console.log('New day, creating new log file');
+        logFilePath = path.join(__dirname, `user_interaction_${dateToday}.log`);
+    }
+
+
+    fs.appendFile(logFilePath, logString, (err) => {
+        if (err) {
+            console.error('Error saving log to file:', err);
+        } else {
+            console.log('Log saved to file:', logFilePath);
+        }
+    });
+}
+    
+const testLogSaveFile = () =>{
+    const log = {
+        dateTime: new Date(),
+        chatId: 123456,
+        userId: 123,
+        userName: 'testuser',
+        messageText: 'test message'
+    }
+    saveLogToFile(log);
 }
 
 module.exports={
