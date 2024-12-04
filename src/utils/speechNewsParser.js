28761b66d3d19403c4fb780c59becf3de9094aac
Mattazz <matthewazada101@gmail.com>
2024-12-04 09:03:46 -0500

    u

--- src/utils/speechNewsParser.js
+++ src/utils/speechNewsParser.js
@@ -24,7 +24,6 @@ async function scrapeSpeechNews(retries = 0) {
 
     const url = "https://www.canada.ca/en/news/advanced-news-search/news-results.html?typ=speeches&dprtmnt=departmentofcitizenshipandimmigration&start=2015-01-01&end="
 
-
     try{
         await page.goto(url, {waitUntil: 'networkidle2', timeout: 60000});
     } catch(error){
